acada7fcb6447888b94083afc2e562a2c53d27cf
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-20 08:32:55 -0700

    Update helloloop.c

--- helloloop.c
+++ helloloop.c
@@ -4,10 +4,17 @@ int main()
 {
     int n;
     scanf("%d",&n);
+    if(n>0)
+    {
     for(int i=0;i<n;i++)
     {
     printf("Hello");
     printf("\n");
     }
+    }
+    else
+    {
     return 0;
+    }
 }
+
