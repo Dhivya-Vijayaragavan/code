48fc489888ad688520261bb1b91b28f040e19414
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-20 07:11:40 -0700

    Update positive.java

--- positive.java
+++ positive.java
@@ -12,15 +12,13 @@ float num;
 
 Scanner sc=new Scanner(System.in);
 
-System.out.println("Enter the Number:");
-
 num=sc.nextFloat();
 
 if(num>0)
 
 {
 
-System.out.println("The Given Number is Positive");
+System.out.println("Positive");
 
 }
 
@@ -28,7 +26,7 @@ else if(num<0)
 
 {
 
-System.out.println("The Given Number is Negative");
+System.out.println("Negative");
 
 }
 
@@ -36,7 +34,7 @@ else
 
 {
 
-System.out.println("The Given Number is Zero");
+System.out.println("Zero");
 
 }
 
