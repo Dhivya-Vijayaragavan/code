d1ff5f07321c4ea700460a402b89a81ca53b51b4
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-20 03:29:15 -0700

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,9 @@
+int num;
+    printf("Enter the Number:");
+    scanf("%d",num);
+    if(num>0)
+    printf("The given number is positive");
+    else if(num<0)
+    printf("The given number is positive");
+    else
+    printf("The Given Number is Zero");
