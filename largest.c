e450bec475faa13c5b3467bae2185ffb50a6f106
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-20 08:05:22 -0700

    Update largest.c

--- largest.c
+++ largest.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-   float x,y,z,largest;
-   scanf("%f %f %f",&x,&y,&z);
+   int x,y,z,largest;
+   scanf("%d %d %d",&x,&y,&z);
    largest=(x>y?(x>z?x:z):(y>z?y:z));
-   printf("%f",largest);
+   printf("%d",largest);
     return 0;
 }
