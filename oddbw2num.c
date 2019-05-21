e66821c471bb1bf7048a886fabf1ce0ffedc142f
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-21 03:52:55 -0700

    Update oddbw2num.c

--- oddbw2num.c
+++ oddbw2num.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
    int start,end,i;
    scanf("%d %d",&start,&end);
-   if(start<end)
-   {
-       for(i=start+1;i<end;i++)
+    for(i=start+1;i<end;i++)
    {
        if(i%2==1)
        {
@@ -14,11 +13,6 @@ int main()
        printf(" ");
        }
    }   
-   }
-   else
-   {
-       printf("give valid boundary");
-   }
    
     return 0;
 }
