24fb24ec1f64e4134387fdcba6ee1343de1f026b
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-21 10:15:13 -0700

    Update ap.c

--- ap.c
+++ ap.c
@@ -4,14 +4,9 @@ int main()
 {
     int n,a,d,ap;
     scanf("%d %d %d",&n,&a,&d);
-    if(1<=n&&a&&d<=100000)
-    {
     ap=(n*(2*a+(n-1)*d))/2;
     printf("%d",ap);
-    }
-    else
-    {
-        printf("enter the n a d value between 1 to 10000");
-    }
+   
     return 0;
 }
+
