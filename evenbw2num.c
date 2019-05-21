a779ae7cd2b7127f2f5137eb15894234d01c8b6f
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-21 03:49:52 -0700

    Create evenbw2num.c

---
+++ evenbw2num.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   int start,end,i;
+   scanf("%d %d",&start,&end);
+    for(i=start+1;i<end;i++)
+   {
+       if(i%2==0)
+       {
+           printf("%d",i);
+           printf(" ");
+       }
+       
+   }   
+    return 0;
+}
