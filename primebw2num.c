78870e0bb96fd3c44d7e5f7cd3d94e7b61a4f419
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-21 04:20:52 -0700

    Create primebw2num.c

---
+++ primebw2num.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int s,e,i,flag=0;
+    scanf("%d %d",&s,&e);
+    s=s+1;
+    while(s<e)
+    {
+        flag=0;
+        for(i=2;i<=s/2;++i)
+    {
+        if(s%i==0)
+        {
+        flag=1;
+        break;
+        }
+        
+    }
+    if(flag==0)
+        printf("%d  ",s);
+        ++s;
+    }
+     return 0;
+}
