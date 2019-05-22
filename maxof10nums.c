086c931e56d828937d68b5c063bb54a5110ae237
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-22 10:27:21 -0700

    Create maxof10nums.c

---
+++ maxof10nums.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=10,a[20],max=0;
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
