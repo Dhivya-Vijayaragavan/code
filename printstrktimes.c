a21bde5f664b1ebef039a1a790fb01134d864883
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-23 23:49:28 -0700

    Create printstrktimes.c

---
+++ printstrktimes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+   
+char str[100];
+   
+int i,k;
+
+scanf("%s",&str);
+ 
+scanf("%d",&k);
+   
+for(i=0;i<k;i++)
+   
+{
+       
+printf("%s",str);
+printf("\n");
+   
+}
+
+    
+return 0;
+
+}
