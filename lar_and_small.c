12419fb87a0770f4600cd3d6ac99ab5ebdd564be
Dhivya-Vijayaragavan <50805497+Dhivya-Vijayaragavan@users.noreply.github.com>
2019-05-24 04:31:27 -0700

    Create lar_and_small.c

---
+++ lar_and_small.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+    int n,i,j,temp,a[50];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+            }
+        }
+    }
+    printf("%d %d",a[0],a[n-1]);
+}
